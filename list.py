ea2db42511a8278f42b4270b7bd5fc52b3b25ece
Terry Sun <terrynsun@gmail.com>
2025-08-01 15:31:09 -0700

    snapshot list

--- list.py
+++ list.py
@@ -6,6 +6,7 @@ import re
 
 from wordlist import Wordlist
 import util
+from util import Color
 
 def print_low_3s(wl: Wordlist):
     compiled_regex = re.compile("...")
@@ -64,11 +65,91 @@ def list_clubs(wl: Wordlist):
     for w in words:
         print(w)
         wl.query_sandwich(w, 40)
+
+        # remove results too short or long
+        # filtered_words = [ x for x in filtered_words if len(x) == 12]
+
         print()
+
+def list_t_to_dos(wl: Wordlist):
+    def match_fn(word):
+        if 'b' in word:
+            new_word = word.replace('b', 'd')
+            return wl.contains(new_word)
+
+    matches = wl.search(match_fn, 50)
+    ws = list(matches.keys())
+    ws = sorted(ws, key=lambda x: len(x))
+    print('\n'.join(ws))
+
+def print_upside_downs(wl: Wordlist):
+    upside_down_dict = {
+        's': 's',
+        'i': 'i',
+        'o': 'o',
+        'n': 'n',
+        'x': 'x',
+        'z': 'z',
+        'h': 'h',
+
+        'm': 'w',
+        'w': 'm',
+    }
+    # 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    def reverse(word):
+        acc = []
+        for letter in word:
+            if letter not in upside_down_dict:
+                return None
+            acc.append(upside_down_dict[letter])
+
+        new_word = ''.join(acc[::-1])
+        return new_word
+
+    def match_fn(word):
+        if len(word) < 4:
+            return False
+
+        new_word = reverse(word)
+        if not new_word:
+            return False
+        return wl.contains(new_word, score_threshold=40)
+
+    matches = wl.search(match_fn, 40)
+    ws = list(matches.keys())
+    ws = sorted(ws, key=lambda x: len(x))
+    for w in ws:
+        print('{} - {}'.format(w, reverse(w)))
+
+def double_cross(wl: Wordlist):
+    compiled_regex = re.compile("sweet.*")
+    regex_match_fn = lambda x: compiled_regex.fullmatch(x)
+
+    matches = wl.search(regex_match_fn, 0)
+    ws = list(matches.keys())
+    for w in ws:
+        suffix = w[5:]
+        double_word = 'sour' + suffix
+        if wl.contains(double_word):
+            print(w, double_word)
+
+def just_add_water(wl: Wordlist):
+    target_word = 'air'
+    compiled_regex = re.compile(".+" + target_word + ".+")
+    regex_match_fn = lambda x: compiled_regex.fullmatch(x)
+
+    matches = wl.search(regex_match_fn, 0)
+    ws = list(matches.keys())
+    for w in ws:
+        new = w.replace(target_word, '')
+        if wl.contains(new):
+            print(Color.highlight(w, target_word, Color.YELLOW), new)
 
 if __name__ == '__main__':
     wl = Wordlist()
-    wl.load('../wordlist/')
+    wl.load('../gh/wordlist/')
 
     #print_matching_halves(wl)
-    list_clubs(wl)
+    #print_upside_downs(wl)
+    #just_add_water(wl)
